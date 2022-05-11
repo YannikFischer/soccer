263cc1f40e805de5fad2e3741b761533787d9f74
FabianT <fabian.tuertscher@student.htldornbirn.at>
2022-05-11 12:43:44 +0200

    init

--- screens/Settings.tsx
+++ screens/Settings.tsx
@@ -1,12 +1,24 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import React from "react";
+
+import GameButton from "../components/GameButton";
 
 const Settings = (props: {}) => {
   return (
     <View>
+      <View style={styles.buttonContainer}>
+          <GameButton
+            onPress={() => props.navigation.navigate("Home")}
+            title="Back"
+          />
+      </View>
       <Text>Settings</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  buttonContainer: {},
+});
 
 export default Settings;
